815274d1f28dd46a912aa9f5e73c87bb6c23baf0
DeeDowns <dee_downs@outlook.com>
2020-09-03 13:08:15 -0400

    made port dynamic; added start script

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const server = require("./api/server.js");
 
-const port = 5000;
+//make port dynamic
+//heroku will add a PORT variable to the environment (the server the app runs on)
+//we can read it from there using the process.env object
+const port = process.env.PORT  ||  5000
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
